cdbe494d27d90607f6350985e042fb91dff0c2b3
zaochuan5854 <zaochuantaiji338@gmail.com>
2026-01-03 08:00:00 +0000

    feat: fbs_galleriesテーブルのデータ圧縮設定を更新
    - fbs_galleriesテーブルのデータカラムに対して圧縮を無効化する設定を追加しました。
    - fbs_compress_typesテーブルに初期データとして'none'と'zstd'を挿入するSQL文を追加しました。

--- sql/fbs_schema.sql
+++ sql/fbs_schema.sql
@@ -7,3 +7,9 @@ CREATE TABLE IF NOT EXISTS fbs_galleries (
     data BYTEA NOT NULL, 
     compress_type INTEGER NOT NULL REFERENCES fbs_compress_types(id)
 );
+
+-- BYTAの圧縮を無効化
+ALTER TABLE fbs_galleries ALTER COLUMN data SET STORAGE EXTERNAL;
+
+-- 圧縮形式の初期データ挿入
+INSERT INTO fbs_compress_types (name) VALUES ('none'), ('zstd') ON CONFLICT DO NOTHING;
